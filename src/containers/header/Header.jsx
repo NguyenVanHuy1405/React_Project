8ae9c9af62456e33ae0216b7697ea98a2aa4c6fe
Huy <84852270+NguyenVanHuy1405@users.noreply.github.com>
2023-03-02 16:41:35 +0700

    “changeall”

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>We’ve trained a model called ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT
+           to answer follow-up questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.</p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your email address"/>
+          <button type="button">Get started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people"/>
+          <p>1600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        {/* <div className="gpt3__header-image">
+          <img src={ai} alt="AI"/>
+        </div> */}
+      </div>
+    </div>
   )
 }
 
